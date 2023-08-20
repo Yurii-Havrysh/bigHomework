b4692a31e15b4df3fcdf2cdda40ff6558a9a3a6f
Yurii Havrysh <gavrisch.yuri@gmail.com>
2023-08-20 21:10:45 +0200

    first commit

--- task2.js
+++ task2.js
@@ -1,23 +1,18 @@
 function highlightKeywords(strings, ...keywords) {
-    return strings.reduce((result, string, i) => {
-      result += string;
-      if (i < keywords.length) {
-        result += `<span class='highlight'>${keywords[i]}</span>`;
-      }
-      return result;
-    }, '');
-  }
-  
-  const keywords = ["JavaScript", "template", "tagged"];
-  const template = (strings, ...values) => {
-    let result = '';
-    strings.forEach((str, i) => {
-      result += str + (values[i] || '');
+    const regex = new RegExp(keywords.join("|"), "gi");
+
+    const highlightedStrings = strings.map((string) => {
+        return string.replace(regex, (match) => {
+            return `<span class='highlight'>${match}</span>`;
+        });
     });
-    return result;
-  };
-  
-  const highlighted = highlightKeywords(template`${0} tagged templates to create custom ${1} literals for ${2} manipulation.`, keywords);
-  
-  console.log(highlighted);
-  // Expected: "Learn <span class='highlight'>JavaScript</span> tagged templates to create custom <span class='highlight'>template</span> literals for <span class='highlight'>tagged</span> manipulation."
+
+    return highlightedStrings.join("");
+}
+
+const keywords = ["JavaScript", "template", "tagged"];
+const template = `Learn \${0} tagged templates to create custom \${1} literals for \${2} manipulation.`;
+
+const highlighted = highlightKeywords(template, ...keywords);
+
+console.log(highlighted);
