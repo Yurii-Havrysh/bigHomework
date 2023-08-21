9b5ba103880b7040c57666da41c0eb359899ebe7
Yurii Havrysh <gavrisch.yuri@gmail.com>
2023-08-21 21:50:11 +0200

    second commit

--- task2.js
+++ task2.js
@@ -1,18 +1,21 @@
-function highlightKeywords(strings, ...keywords) {
-    const regex = new RegExp(keywords.join("|"), "gi");
-
-    const highlightedStrings = strings.map((string) => {
-        return string.replace(regex, (match) => {
-            return `<span class='highlight'>${match}</span>`;
-        });
+function highlightKeywords(strings, ...keys) {
+    const keywords = keys.map(keyword => `<span class='highlight'>${keyword}</span>`);
+    let result = '';
+  
+    strings.forEach((string, i) => {
+        result += string;
+        if (i < keywords.length) {
+        result += keywords[i];
+        }
     });
-
-    return highlightedStrings.join("");
+  
+    return result;
 }
-
+  
 const keywords = ["JavaScript", "template", "tagged"];
-const template = `Learn \${0} tagged templates to create custom \${1} literals for \${2} manipulation.`;
-
-const highlighted = highlightKeywords(template, ...keywords);
-
+const template = "Learn ${0} tagged templates to create custom ${1} literals for ${2} manipulation.";
+  
+const highlighted = highlightKeywords`Learn ${keywords[0]} tagged templates to create custom ${keywords[1]} literals for ${keywords[2]} manipulation.`;
+  
 console.log(highlighted);
+// Expected: "Learn <span class='highlight'>JavaScript</span> tagged templates to create custom <span class='highlight'>template</span> literals for <span class='highlight'>tagged</span> manipulation."
